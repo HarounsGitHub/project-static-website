cc4c44e0c61c25376b29a7d61b2df8832a14d295
HarounsGitHub <benmahdjoub.haroun@gmail.com>
2023-06-04 23:47:29 +0100

    better ux by recorrecting the input bar again &  improving responsiveness overall

---
+++ src/components/popCard/popCard.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "./popCard.scss";
+
+import React from "react";
+
+const popCard = () => {
+  return (
+    <Link>
+      <div className="popCard"></div>
+    </Link>
+  );
+};
+
+export default popCard;
---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const cardsData = [
+  {
+    id: 1,
+    title: "Teriyaki",
+    desc: "Traditional technique",
+    img: "https://cdn.tasteatlas.com//Images/Dishes/2412f557af2d415cb8f177ee2a1cca62.jpg?w=905&h=510",
+  },
+  {
+    id: 2,
+    title: "Churrasco",
+    desc: "Authentic recipe",
+    img: "https://cdn.tasteatlas.com/images/dishes/3571507db7da42a49b116d559e68fb58.jpg?w=905&h=510",
+  },
+  {
+    id: 3,
+    title: "Guacamole",
+    desc: "Authentic recipe",
+    img: "https://cdn.tasteatlas.com/images/dishes/1c79ac0beb2242419a445ae0a51d3acc.jpg?mw=1300",
+  },
+  {
+    id: 4,
+    title: "Paella",
+    desc: "Rice dish",
+    img: "https://cdn.tasteatlas.com/images/dishes/0b456cded8e245acb83036114c806091.jpg?mw=1300",
+  },
+  {
+    id: 5,
+    title: "Chutney",
+    desc: "Dip dish",
+    img: "https://cdn.tasteatlas.com/images/dishes/0c65a7fc0cc747f0b124c027bb4f14f8.jpg?mw=1300",
+  },
+  {
+    id: 6,
+    title: "Binagoongan",
+    desc: "Traditional dish",
+    img: "https://cdn.tasteatlas.com/images/dishes/77efd9035a0d43839a16184808e2a7ca.jpg?mw=1300",
+  },
+  {
+    id: 7,
+    title: "Asado",
+    desc: "Barbecue",
+    img: "https://cdn.tasteatlas.com/Images/Dishes/bc210d52370b456092dde323280efbb7.jpg?mw=1300",
+  },
+  {
+    id: 8,
+    title: "Tiramisù",
+    desc: "Dessert",
+    img: "https://cdn.tasteatlas.com/images/dishes/468fa931d3004dd4b20f567cfb37c8b8.jpg?mw=1300",
+  },
+];
