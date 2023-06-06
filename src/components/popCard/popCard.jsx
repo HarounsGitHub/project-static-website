79ea31e0f9d59284152f5aae2432fe21a276b00b
HarounsGitHub <benmahdjoub.haroun@gmail.com>
2023-06-06 14:45:11 +0100

    better card text lisibility on bright background images

--- src/components/popCard/popCard.jsx
+++ src/components/popCard/popCard.jsx
@@ -1,14 +1,19 @@
-import { Link } from "react-router-dom";
-import "./popCard.scss";
-
 import React from "react";
+import { Link } from "react-router-dom";
+import "./PopCard.scss";
+// import { cards } from "../../data";
+// import Slide from "../slide/Slide";
 
-const popCard = () => {
+function PopCard({ card }) {
   return (
-    <Link>
-      <div className="popCard"></div>
+    <Link to="/gigs?cat=dessert">
+      <div className="popCard">
+        <img src={card.img} alt="" />
+        <div className="overlay"></div>
+        <span className="desc">{card.desc}</span>
+        <span className="title">{card.title}</span>
+      </div>
     </Link>
   );
-};
-
-export default popCard;
+}
+export default PopCard;
