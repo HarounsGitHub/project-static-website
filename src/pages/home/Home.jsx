be9216790a354d1a0c19d1910fdf2088b4fa580a
HarounsGitHub <benmahdjoub.haroun@gmail.com>
2023-06-11 23:36:35 +0100

    cleaner home code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Banner from "../../components/banner/Banner";
 import Partners from "../../components/partners/Partners";
 import Slide from "../../components/slide/Slide";
@@ -40,41 +39,6 @@ const Home = () => {
         ))}
       </Slide>
       <Partners />
-      {/* <Spotlight /> */}
-      {/* <Collage /> */}
-      {/* <div className="collage">
-        <img
-          className="collage-row-2"
-          src="https://cdn.tasteatlas.com/images/regions/8f928125b6c944f887467d5b5535e0c2.jpg"
-          alt=""
-        />
-        <img
-          src="https://cdn.tasteatlas.com/Images/Dishes/9a9d8260bd2647dc946047bacc9aa57c.jpg"
-          alt=""
-        />
-        <img
-          src="https://cdn.tasteatlas.com/images/ingredients/b3d83fe75c5342518f4396885f63ac72.jpg"
-          alt=""
-        />
-        <img
-          src="https://cdn.tasteatlas.com/Images/Regions/78684a5482d24820b83e479ae457d7eb.jpg"
-          alt=""
-        />
-        <img
-          src="https://cdn.tasteatlas.com/images/regions/1771dd25eec14815a899b4b640fa0f24.jpg"
-          alt=""
-        />
-        <img
-          className="collage-row-2"
-          src="https://cdn.tasteatlas.com/images/regions/8f928125b6c944f887467d5b5535e0c2.jpg"
-          alt=""
-        />
-        <img
-          className="collage-row-2"
-          src="https://cdn.tasteatlas.com/images/regions/8f928125b6c944f887467d5b5535e0c2.jpg"
-          alt=""
-        />
-      </div> */}
       <Slide slidesToShow={slidesToShow} arrowsScroll={slidesToShow}>
         {recipes.map((item) => (
           <RecipeCard key={item.id} item={item} />
