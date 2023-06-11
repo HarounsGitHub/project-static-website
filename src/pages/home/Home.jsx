45010da880a43d5e5f57d6537d587ee0e163368c
HarounsGitHub <benmahdjoub.haroun@gmail.com>
2023-06-11 23:35:09 +0100

    best responsivity of home of component & implement new recipe card component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,24 +2,46 @@ import React from "react";
 import Banner from "../../components/banner/Banner";
 import Partners from "../../components/partners/Partners";
 import Slide from "../../components/slide/Slide";
-import { cards } from "../../data";
+import { cards, recipes } from "../../data";
 import PopCard from "../../components/popCard/popCard";
 import "./Home.scss";
-import Spotlight from "../../components/spotlight/Spotlight";
-import Collage from "../../components/collage/Collage";
+import RecipeCard from "../../components/recipeCard/RecipeCard";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [slidesToShow, setSlidesToShow] = useState(4);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 480) {
+        setSlidesToShow(1);
+      } else if (window.innerWidth <= 768) {
+        setSlidesToShow(2);
+      } else {
+        setSlidesToShow(4);
+      }
+    };
+
+    // Call handleResize on initial load
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up event listener on component unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <div className="home">
       <Banner />
-      <Slide slidesToShow={5} arrowsScroll={5}>
+      <Slide slidesToShow={slidesToShow} arrowsScroll={slidesToShow}>
         {cards.map((card) => (
           <PopCard key={card.id} card={card} />
         ))}
       </Slide>
       <Partners />
       {/* <Spotlight /> */}
-      <Collage />
+      {/* <Collage /> */}
       {/* <div className="collage">
         <img
           className="collage-row-2"
@@ -53,6 +75,11 @@ const Home = () => {
           alt=""
         />
       </div> */}
+      <Slide slidesToShow={slidesToShow} arrowsScroll={slidesToShow}>
+        {recipes.map((item) => (
+          <RecipeCard key={item.id} item={item} />
+        ))}
+      </Slide>
     </div>
   );
 };
